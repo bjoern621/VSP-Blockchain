4cc0c855549ec9b755d2906ce330e2ffaf2726e8
Bennet <bennetkrzenzck@pm.me>
2025-12-06 16:12:44 +0100

    add attach/detach

--- p2p-blockchain/netzwerkrouting/infrastructure/middleware/grpc/observer/ObservableBlockchainServer.go
+++ p2p-blockchain/netzwerkrouting/infrastructure/middleware/grpc/observer/ObservableBlockchainServer.go
@@ -3,5 +3,8 @@ package observer
 import "s3b/vsp-blockchain/p2p-blockchain/blockchain"
 
 type ObservableBlockchainServer interface {
+	Attach(o *BlockchainObserver) (bool, error)
+	Detach(o *BlockchainObserver) (bool, error)
+
 	NotifyInv(invMsg blockchain.InvMsg)
 }
