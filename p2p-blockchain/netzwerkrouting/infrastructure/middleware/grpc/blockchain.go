9416a283099b5e4fe3b9a86bf3ec0237d61e77f2
Bennet <bennetkrzenzck@pm.me>
2025-12-06 16:58:17 +0100

    implemented conversion for data and call of inv via observer

--- p2p-blockchain/netzwerkrouting/infrastructure/middleware/grpc/blockchain.go
+++ p2p-blockchain/netzwerkrouting/infrastructure/middleware/grpc/blockchain.go
@@ -2,14 +2,53 @@ package grpc
 
 import (
 	"context"
+	"fmt"
+	"s3b/vsp-blockchain/p2p-blockchain/blockchain"
 	"s3b/vsp-blockchain/p2p-blockchain/internal/pb"
 
+	"bjoernblessin.de/go-utils/util/assert"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+func bytesToHash(bytes []byte) (*blockchain.Hash, error) {
+	var hash blockchain.Hash
+	if len(bytes) != 32 {
+		return &hash, fmt.Errorf("invalid hash length: %d", len(bytes))
+	}
+
+	copy(hash[:], bytes)
+	return &hash, nil
+}
+
+func fromProtoInvMsg(msg *pb.InvMsg) blockchain.InvMsg {
+	invVector := make([]*blockchain.InvVector, len(msg.Inventory))
+	for _, pbInvVector := range msg.Inventory {
+		hash, err := bytesToHash(pbInvVector.Hash)
+		assert.IsNil(err, "failed to convert hash from proto")
+
+		invVector = append(invVector, &blockchain.InvVector{
+			InvType: blockchain.InvType(pbInvVector.Type),
+			Hash:    hash,
+		})
+	}
+
+	invMsg := blockchain.InvMsg{
+		Inventory: invVector,
+	}
+	return invMsg
+}
+
 func (s *Server) Inv(ctx context.Context, msg *pb.InvMsg) (*emptypb.Empty, error) {
-	//TODO implement me
-	panic("implement me")
+	invMsg := fromProtoInvMsg(msg)
+	go s.NotifyInv(invMsg)
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *Server) NotifyInv(invMsg blockchain.InvMsg) {
+	for observer := range s.observers {
+		observer.Inv(invMsg)
+	}
 }
 
 func (s *Server) GetData(ctx context.Context, msg *pb.GetDataMsg) (*emptypb.Empty, error) {
