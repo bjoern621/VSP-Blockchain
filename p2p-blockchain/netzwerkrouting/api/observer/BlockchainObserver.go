f42e43b4251a1ae07b59d59bd1defe492e9fcf41
Bennet <bennetkrzenzck@pm.me>
2025-12-07 01:30:53 +0100

    add some documentation

--- p2p-blockchain/netzwerkrouting/api/observer/BlockchainObserver.go
+++ p2p-blockchain/netzwerkrouting/api/observer/BlockchainObserver.go
@@ -2,6 +2,11 @@ package observer
 
 import "s3b/vsp-blockchain/p2p-blockchain/blockchain"
 
+// BlockchainObserver defines the interface for a blockchain observer.
+// A blockchain observer is somebody interested in blockchain events.
+// A blockchain observer shall be attached to a blockchain server via the ObservableBlockchainServer.Attach() method
+// and be removed via the ObservableBlockchainServer.Detach() method on a valid ObservableBlockchainServer.
+// A blockchain observer shall implement the corresponding methods to handle blockchain events.
 type BlockchainObserver interface {
 	Inv(invMsg *blockchain.InvMsg)
 	GetData(getDataMsg *blockchain.GetDataMsg)
--- p2p-blockchain/netzwerkrouting/api/observer/ObservableBlockchainServer.go
+++ p2p-blockchain/netzwerkrouting/api/observer/ObservableBlockchainServer.go
@@ -4,9 +4,14 @@ import (
 	"s3b/vsp-blockchain/p2p-blockchain/blockchain"
 )
 
+// ObservableBlockchainServer defines the interface for an observable blockchain server.
+// A new observer shall be attached to the server via the Attach() method and be removed via the Detach() method.
+// The server shall also implement the corresponding methods to notify observers about changes.
 type ObservableBlockchainServer interface {
-	Attach(o *BlockchainObserver)
-	Detach(o *BlockchainObserver)
+	// Attach is called by the observer to attach itself to the server.
+	Attach(o BlockchainObserver)
+	// Detach is called by the observer to detach itself from the server.
+	Detach(o BlockchainObserver)
 
 	NotifyInv(invMsg *blockchain.InvMsg)
 	NotifyGetData(getDataMsg *blockchain.GetDataMsg)
