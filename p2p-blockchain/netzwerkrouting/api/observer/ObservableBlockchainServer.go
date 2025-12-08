bf5bb53ccac84c8f77153309af039aa09b9cf098
Bennet <bennetkrzenzck@pm.me>
2025-12-08 22:48:33 +0100

    add peerID to ObservableBlockChainServerAPI methods

--- p2p-blockchain/netzwerkrouting/api/observer/ObservableBlockchainServer.go
+++ p2p-blockchain/netzwerkrouting/api/observer/ObservableBlockchainServer.go
@@ -2,6 +2,7 @@ package observer
 
 import (
 	"s3b/vsp-blockchain/p2p-blockchain/blockchain"
+	"s3b/vsp-blockchain/p2p-blockchain/netzwerkrouting/core/peer"
 )
 
 // ObservableBlockchainServerAPI defines the interface for an observable blockchain server.
@@ -13,13 +14,13 @@ type ObservableBlockchainServerAPI interface {
 	// Detach is called by the observer to detach itself from the server.
 	Detach(o BlockchainObserverAPI)
 
-	NotifyInv(invMsg *blockchain.InvMsg)
-	NotifyGetData(getDataMsg *blockchain.GetDataMsg)
-	NotifyBlock(blockMsg *blockchain.BlockMsg)
-	NotifyMerkleBlock(merkleBlockMsg *blockchain.MerkleBlockMsg)
-	NotifyTx(txMsg *blockchain.TxMsg)
-	NotifyGetHeaders(blockLocator *blockchain.BlockLocator)
-	NotifyHeaders(headers []*blockchain.BlockHeader)
-	NotifySetFilterRequest(setFilterRequest *blockchain.SetFilterRequest)
-	NotifyMempool()
+	NotifyInv(invMsg *blockchain.InvMsg, peerID peer.PeerID)
+	NotifyGetData(getDataMsg *blockchain.GetDataMsg, peerID peer.PeerID)
+	NotifyBlock(blockMsg *blockchain.BlockMsg, peerID peer.PeerID)
+	NotifyMerkleBlock(merkleBlockMsg *blockchain.MerkleBlockMsg, peerID peer.PeerID)
+	NotifyTx(txMsg *blockchain.TxMsg, peerID peer.PeerID)
+	NotifyGetHeaders(blockLocator *blockchain.BlockLocator, peerID peer.PeerID)
+	NotifyHeaders(headers []*blockchain.BlockHeader, peerID peer.PeerID)
+	NotifySetFilterRequest(setFilterRequest *blockchain.SetFilterRequest, peerID peer.PeerID)
+	NotifyMempool(peerID peer.PeerID)
 }
