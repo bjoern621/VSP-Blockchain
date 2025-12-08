7a72ef99315d52960066b4e28b85c7edd10637b8
Bennet <bennetkrzenzck@pm.me>
2025-12-08 22:48:49 +0100

    add peerID to BlockchainObserverAPI methods

--- p2p-blockchain/netzwerkrouting/api/observer/BlockchainObserver.go
+++ p2p-blockchain/netzwerkrouting/api/observer/BlockchainObserver.go
@@ -1,6 +1,9 @@
 package observer
 
-import "s3b/vsp-blockchain/p2p-blockchain/blockchain"
+import (
+	"s3b/vsp-blockchain/p2p-blockchain/blockchain"
+	"s3b/vsp-blockchain/p2p-blockchain/netzwerkrouting/core/peer"
+)
 
 // BlockchainObserverAPI defines the interface for a blockchain observer.
 // A blockchain observer is somebody interested in blockchain events.
@@ -8,13 +11,13 @@ import "s3b/vsp-blockchain/p2p-blockchain/blockchain"
 // and be removed via the ObservableBlockchainServerAPI.Detach() method on a valid ObservableBlockchainServerAPI.
 // A blockchain observer shall implement the corresponding methods to handle blockchain events.
 type BlockchainObserverAPI interface {
-	Inv(invMsg *blockchain.InvMsg)
-	GetData(getDataMsg *blockchain.GetDataMsg)
-	Block(blockMsg *blockchain.BlockMsg)
-	MerkleBlock(merkleBlockMsg *blockchain.MerkleBlockMsg)
-	Tx(txMsg *blockchain.TxMsg)
-	GetHeaders(locator *blockchain.BlockLocator)
-	Headers(headers []*blockchain.BlockHeader)
-	SetFilter(setFilterRequest *blockchain.SetFilterRequest)
-	Mempool()
+	Inv(invMsg *blockchain.InvMsg, peerID peer.PeerID)
+	GetData(getDataMsg *blockchain.GetDataMsg, peerID peer.PeerID)
+	Block(blockMsg *blockchain.BlockMsg, peerID peer.PeerID)
+	MerkleBlock(merkleBlockMsg *blockchain.MerkleBlockMsg, peerID peer.PeerID)
+	Tx(txMsg *blockchain.TxMsg, peerID peer.PeerID)
+	GetHeaders(locator *blockchain.BlockLocator, peerID peer.PeerID)
+	Headers(headers []*blockchain.BlockHeader, peerID peer.PeerID)
+	SetFilter(setFilterRequest *blockchain.SetFilterRequest, peerID peer.PeerID)
+	Mempool(peerID peer.PeerID)
 }
