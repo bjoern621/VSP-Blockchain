469f096596bf7618c7e48de9bbea153c1d1eaef4
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-09 23:35:08 +0100

    Refactored architecture tests to implement dependency rules for subsystems

--- p2p-blockchain/internal/architecture_test/architecture_test.go
+++ p2p-blockchain/internal/architecture_test/architecture_test.go
@@ -2,6 +2,7 @@ package architecture_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	archgo "github.com/arch-go/arch-go/api"
@@ -9,26 +10,53 @@ import (
 )
 
 func TestArchitecture(t *testing.T) {
+	subsystems := []string{"netzwerkrouting", "wallet", "miner", "blockchain", "app"}
+	var rules []*config.DependenciesRule
+
+	for _, s := range subsystems {
+		// API Rule
+		// api can depend on: core (same subsystem), api (other subsystems)
+		allowedApiDeps := []string{
+			fmt.Sprintf("**.%s.api.**", s),
+			fmt.Sprintf("**.%s.core.**", s),
+		}
+		// Add other subsystems' APIs
+		for _, other := range subsystems {
+			if other != s {
+				allowedApiDeps = append(allowedApiDeps, fmt.Sprintf("**.%s.api.**", other))
+			}
+		}
+
+		rules = append(rules, &config.DependenciesRule{
+			Package: fmt.Sprintf("**.%s.api.**", s),
+			ShouldOnlyDependsOn: &config.Dependencies{
+				Internal: allowedApiDeps,
+			},
+		})
+
+		// Core Rule
+		// core can depend on: data (same subsystem), api (other subsystems)
+		allowedCoreDeps := []string{
+			fmt.Sprintf("**.%s.core.**", s),
+			fmt.Sprintf("**.%s.data.**", s),
+			"**.p2p-blockchain.internal.common.**",
+		}
+		for _, other := range subsystems {
+			if other != s {
+				allowedCoreDeps = append(allowedCoreDeps, fmt.Sprintf("**.%s.api.**", other))
+			}
+		}
+
+		rules = append(rules, &config.DependenciesRule{
+			Package: fmt.Sprintf("**.%s.core.**", s),
+			ShouldOnlyDependsOn: &config.Dependencies{
+				Internal: allowedCoreDeps,
+			},
+		})
+	}
+
 	configuration := config.Config{
-		DependenciesRules: []*config.DependenciesRule{
-			{
-				Package: "**.core.**",
-				ShouldNotDependsOn: &config.Dependencies{
-					Internal: []string{
-						"**.infrastructure.**",
-						"**.api.**",
-					},
-				},
-			},
-			{
-				Package: "**.netzwerkrouting.**",
-				ShouldNotDependsOn: &config.Dependencies{
-					Internal: []string{
-						"**.blockchain.**",
-					},
-				},
-			},
-		},
+		DependenciesRules: rules,
 	}
 	moduleInfo := config.Load("s3b/vsp-blockchain/p2p-blockchain")
 
