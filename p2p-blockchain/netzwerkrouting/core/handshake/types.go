e89b192109548550570ff94b84022e45399f827f
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-16 21:46:01 +0100

    removed unneeded functiosn

--- p2p-blockchain/netzwerkrouting/core/handshake/types.go
+++ p2p-blockchain/netzwerkrouting/core/handshake/types.go
@@ -5,7 +5,6 @@ import (
 	"s3b/vsp-blockchain/p2p-blockchain/netzwerkrouting/core/peer"
 
 	"bjoernblessin.de/go-utils/util/assert"
-	"bjoernblessin.de/go-utils/util/logger"
 )
 
 type VersionInfo struct {
@@ -54,27 +53,6 @@ func (v *VersionInfo) addService(svc peer.ServiceType) {
 	v.supportedServices = append(v.supportedServices, svc)
 }
 
-// ValidateServices checks that the current SupportedServices satisfy all domain rules.
-// Call this after all services have been added.
-func (v *VersionInfo) ValidateServices() {
-	hasBlockchain := false
-	for _, svc := range v.supportedServices {
-		if svc == peer.ServiceType_BlockchainFull || svc == peer.ServiceType_BlockchainSimple {
-			hasBlockchain = true
-			break
-		}
-	}
-
-	for _, svc := range v.supportedServices {
-		if svc == peer.ServiceType_Wallet {
-			assert.Assert(hasBlockchain, "wallet requires blockchain_full or blockchain_simple")
-		}
-		if svc == peer.ServiceType_Miner {
-			assert.Assert(hasBlockchain, "miner requires blockchain_full or blockchain_simple")
-		}
-	}
-}
-
 // handshakeService implements HandshakeMsgHandler (for infrastructure) and HandshakeInitiator (for api) with the actual domain logic.
 type handshakeService struct {
 	handshakeMsgSender HandshakeMsgSender
@@ -94,16 +72,11 @@ func NewLocalVersionInfo() VersionInfo {
 		Version: common.VersionString,
 	}
 
-	logger.Warnf("systems %v", common.EnabledTeilsystemeNames())
-
 	for _, svcString := range common.EnabledTeilsystemeNames() {
 		svc, ok := peer.ParseServiceType(svcString)
-		logger.Warnf("adding service %v", svc)
 		assert.Assert(ok, "enabled service is not a valid ServiceType:", svcString)
 		info.AddService(svc)
 	}
-
-	info.ValidateServices()
 
 	return info
 }
