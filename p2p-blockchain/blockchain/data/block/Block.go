22949da75de84711314e81baa4d5690199b8bf2a
Björn <41452212+bjoern621@users.noreply.github.com>
2025-12-17 19:51:07 +0100

    comment

--- p2p-blockchain/blockchain/data/block/Block.go
+++ p2p-blockchain/blockchain/data/block/Block.go
@@ -5,6 +5,8 @@ import (
 	"s3b/vsp-blockchain/p2p-blockchain/netzwerkrouting/api/blockchain/dto"
 )
 
+// Block represents a block in the blockchain.
+// It consists of a BlockHeader and a list of Transactions.
 type Block struct {
 	Header       BlockHeader
 	Transactions []transaction.Transaction
