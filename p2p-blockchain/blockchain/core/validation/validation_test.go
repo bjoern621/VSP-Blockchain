6b037ba34b3f6120aad57da3e1c79c53372e2fca
Bennet Krzenzck <bennetkrzenzck@pm.me>
2025-12-19 14:09:30 +0100

    fixed the tets

--- p2p-blockchain/blockchain/core/validation/validation_test.go
+++ p2p-blockchain/blockchain/core/validation/validation_test.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 	"testing"
 
-	"s3b/vsp-blockchain/p2p-blockchain/blockchain/data/transaction"
+	"s3b/vsp-blockchain/p2p-blockchain/internal/common/data/transaction"
 )
 
 type MockUTXOService struct {
--- p2p-blockchain/internal/common/data/block/BlockHeader.go
+++ p2p-blockchain/internal/common/data/block/BlockHeader.go
@@ -3,9 +3,9 @@ package block
 import "s3b/vsp-blockchain/p2p-blockchain/internal/common"
 
 type BlockHeader struct {
-	Hash             common.Hash
-	MerkleRoot       common.Hash
-	Timestamp        int64
-	DifficultyTarget uint32
-	Nonce            uint32
+	PreviousBlockHash common.Hash
+	MerkleRoot        common.Hash
+	Timestamp         int64
+	DifficultyTarget  uint32
+	Nonce             uint32
 }
