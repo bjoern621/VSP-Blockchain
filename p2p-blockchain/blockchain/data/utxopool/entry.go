ce80397fc6080fa96b094c4092d37e5d7d4742fa
bjarne-rtjn <bjarne.rathjen@outlook.de>
2025-12-21 00:22:50 +0100

    added data types for utxo pool

---
+++ p2p-blockchain/blockchain/data/utxopool/entry.go
@@ -0,0 +1,26 @@
+package utxopool
+
+import (
+	"s3b/vsp-blockchain/p2p-blockchain/blockchain/data/transaction"
+)
+
+// UTXOEntry represents a stored UTXO with metadata
+type UTXOEntry struct {
+	Output      transaction.Output
+	BlockHeight uint64 // 0 for unconfirmed transactions
+	IsCoinbase  bool
+}
+
+// NewUTXOEntry creates a new UTXO entry
+func NewUTXOEntry(output transaction.Output, blockHeight uint64, isCoinbase bool) UTXOEntry {
+	return UTXOEntry{
+		Output:      output,
+		BlockHeight: blockHeight,
+		IsCoinbase:  isCoinbase,
+	}
+}
+
+// IsConfirmed returns true if this UTXO is from a confirmed block
+func (e UTXOEntry) IsConfirmed() bool {
+	return e.BlockHeight > 0
+}
