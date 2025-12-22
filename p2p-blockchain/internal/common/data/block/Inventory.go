085853d32eb230adffacc54116921ff0374d2882
Bennet <bennetkrzenzck@pm.me>
2025-12-22 01:53:29 +0100

    make enum

--- p2p-blockchain/internal/common/data/block/Inventory.go
+++ p2p-blockchain/internal/common/data/block/Inventory.go
@@ -5,9 +5,9 @@ import "s3b/vsp-blockchain/p2p-blockchain/internal/common"
 type InvType int
 
 const (
-	InvTypeMsgTx            InvType = 0
-	InvTypeMsgBlock         InvType = 1
-	InvTypeMsgFilteredBlock InvType = 2
+	InvTypeMsgTx InvType = iota
+	InvTypeMsgBlock
+	InvTypeMsgFilteredBlock
 )
 
 type InvVector struct {
