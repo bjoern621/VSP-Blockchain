f8373c45197aa6e38d8414fdf2b36a6e66b7dac0
Bennet <bennetkrzenzck@pm.me>
2025-12-23 14:11:52 +0100

    grobe struktur geschaffen

--- p2p-blockchain/miner/miner.go
+++ p2p-blockchain/miner/miner.go
@@ -1,1 +1,29 @@
 package miner
+
+import (
+	"s3b/vsp-blockchain/p2p-blockchain/internal/common/data/block"
+	"s3b/vsp-blockchain/p2p-blockchain/internal/common/data/transaction"
+)
+
+type MinerAPI interface {
+	CreateCandidateBlock() block.Block
+	MineBlock(candidateBlock block.Block) block.Block
+}
+
+type MinerService struct {
+}
+
+func (m *MinerService) CreateCandidateBlock(transactions []transaction.Transaction) block.Block {
+	header := createCandidateBlockHeader()
+
+	return block.Block{Header: header}
+}
+
+func createCandidateBlockHeader() block.BlockHeader {
+	return block.BlockHeader{}
+}
+
+func (m *MinerService) MineBlock(candidateBlock block.Block) block.Block {
+
+	return block.Block{}
+}
