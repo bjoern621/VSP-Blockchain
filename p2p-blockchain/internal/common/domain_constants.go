f8062efabbcfb2787748a8e85d96fb042bf094b1
bjarne-rtjn <bjarne.rathjen@outlook.de>
2026-01-10 15:50:28 +0100

    ran gofmt

--- p2p-blockchain/internal/common/domain_constants.go
+++ p2p-blockchain/internal/common/domain_constants.go
@@ -5,6 +5,6 @@ const (
 	PublicKeyHashSize = 20
 	PublicKeySize     = 33
 	// HashSize defines the size of (1) block-, (2) transaction- and (3) merkle root-hashes in bytes.
-	HashSize = 32
-	TransactionFee    uint64 = 1
+	HashSize              = 32
+	TransactionFee uint64 = 1
 )
