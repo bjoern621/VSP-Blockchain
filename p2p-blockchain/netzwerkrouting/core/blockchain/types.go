51e57608600c6a5cab1dc69dacf0438e678e1a7d
Bennet <bennetkrzenzck@pm.me>
2026-01-10 19:55:54 +0100

    finally fix ai

--- p2p-blockchain/netzwerkrouting/core/blockchain/types.go
+++ p2p-blockchain/netzwerkrouting/core/blockchain/types.go
@@ -1,14 +1,12 @@
 package blockchain
 
 import (
-	"s3b/vsp-blockchain/p2p-blockchain/blockchain/data/blockchain"
 	"s3b/vsp-blockchain/p2p-blockchain/netzwerkrouting/core/peer"
 )
 
 type BlockchainService struct {
 	blockchainMsgSender BlockchainMsgSender
 	peerStore           *peer.PeerStore
-	blockStore          *blockchain.BlockStore
 }
 
 func NewBlockchainService(blockchainMsgSender BlockchainMsgSender, peerStore *peer.PeerStore) *BlockchainService {
