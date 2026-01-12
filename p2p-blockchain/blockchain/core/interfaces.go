fb2ea43647912c39263525d9a6079883888c71fe
Bennet <bennetkrzenzck@pm.me>
2026-01-12 23:05:25 +0100

    fix type move

--- p2p-blockchain/blockchain/core/interfaces.go
+++ p2p-blockchain/blockchain/core/interfaces.go
@@ -2,13 +2,8 @@ package core
 
 import (
 	"s3b/vsp-blockchain/p2p-blockchain/blockchain/data/blockchain"
-	"s3b/vsp-blockchain/p2p-blockchain/blockchain/data/utxo"
 )
 
 type BlockStoreAPI interface {
 	blockchain.BlockStoreAPI
-}
-
-type UtxoServiceAPI interface {
-	utxo.UTXOService
 }
