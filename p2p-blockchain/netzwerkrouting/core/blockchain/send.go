b0b27872f532afbbaadadec4910bf3ee27bfe22c
BennetK <77968577+BennetKrz@users.noreply.github.com>
2026-01-15 18:51:22 +0100

    202 us mempool (#205)

--- p2p-blockchain/netzwerkrouting/core/blockchain/send.go
+++ p2p-blockchain/netzwerkrouting/core/blockchain/send.go
@@ -5,6 +5,8 @@ import (
 	"s3b/vsp-blockchain/p2p-blockchain/internal/common/data/block"
 	"s3b/vsp-blockchain/p2p-blockchain/internal/common/data/inv"
 	"slices"
+
+	"bjoernblessin.de/go-utils/util/assert"
 )
 
 type BlockchainMsgSender interface {
@@ -21,10 +23,15 @@ type BlockchainMsgSender interface {
 // SendGetData sends a getdata message to the given peer
 func (b *BlockchainService) SendGetData(inventory []*inv.InvVector, peerId common.PeerId) {
 	_, ok := b.peerRetriever.GetPeer(peerId)
-	if !ok {
-		panic("peer '" + peerId + "' not found")
-	}
+	assert.Assert(ok, "peer '"+peerId+"' not found")
 	b.blockchainMsgSender.SendGetData(inventory, peerId)
+}
+
+// SendInv sends an inv message to the given peer
+func (b *BlockchainService) SendInv(inventory []*inv.InvVector, peerId common.PeerId) {
+	_, ok := b.peerRetriever.GetPeer(peerId)
+	assert.Assert(ok, "peer '"+peerId+"' not found")
+	b.blockchainMsgSender.SendInv(inventory, peerId)
 }
 
 // BroadcastInvExclusionary propagates an inventory message to all outbound peers except the specified peer.
