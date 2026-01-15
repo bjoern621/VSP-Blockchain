1a58b645513edd1695443e277966b2043920638f
Bennet Krzenzck <bennetkrzenzck@pm.me>
2026-01-15 19:27:13 +0100

    reference docu

--- p2p-blockchain/blockchain/core/get_headers_handler.go
+++ p2p-blockchain/blockchain/core/get_headers_handler.go
@@ -38,7 +38,7 @@ func (b *Blockchain) GetHeaders(locator block.BlockLocator, peerID common.PeerId
 	}
 
 	// Collect headers starting from the block after the common ancestor
-	// Maximum 100 headers as we will most likely never have a blockchain this long...block
+	// Maximum 100 headers as specified in the protocol (blockchain.proto)
 	headers := b.collectBlockHeaders(locator, commonAncestorHeight)
 
 	// Send headers back to the requesting peer
