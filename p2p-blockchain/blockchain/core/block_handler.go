e5576f057a378f3e0229e590fe627a409ab03fc8
Bennet <bennetkrzenzck@pm.me>
2026-01-17 12:37:19 +0100

    move constant for error message to block_handler.go file

--- p2p-blockchain/blockchain/core/block_handler.go
+++ p2p-blockchain/blockchain/core/block_handler.go
@@ -8,6 +8,8 @@ import (
 	"bjoernblessin.de/go-utils/util/logger"
 )
 
+const invalidBlockMessageFormat = "Block Message received from %v is invalid: %v"
+
 func (b *Blockchain) Block(receivedBlock block.Block, peerID common.PeerId) {
 	logger.Infof("[block_handler] Block Message %v received from %v with %d transactions", &receivedBlock.Header,
 		peerID, len(receivedBlock.Transactions))
