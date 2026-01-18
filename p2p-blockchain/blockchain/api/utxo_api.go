fec86b0a878fe2a825ad9c74545f8d576353a75d
Bennet <bennetkrzenzck@pm.me>
2026-01-18 03:25:54 +0100

    refactor: simplify UtxoAPI structure and introduce UtxoStoreAPI interface

--- p2p-blockchain/blockchain/api/utxo_api.go
+++ p2p-blockchain/blockchain/api/utxo_api.go
@@ -2,19 +2,8 @@ package api
 
 import (
 	"s3b/vsp-blockchain/p2p-blockchain/blockchain/core/utxo"
-	"s3b/vsp-blockchain/p2p-blockchain/internal/common/data/transaction"
 )
 
-type UtxoAPI struct {
-	utxo.LookupService
-}
-
-func NewUtxoAPI(lookupService utxo.LookupService) *UtxoAPI {
-	return &UtxoAPI{
-		LookupService: lookupService,
-	}
-}
-
-func (u *UtxoAPI) GetUTXOsByPubKeyHash(pubKeyHash transaction.PubKeyHash) ([]transaction.UTXO, error) {
-	return u.LookupService.GetUTXOsByPubKeyHash(pubKeyHash)
+type UtxoStoreAPI interface {
+	utxo.UtxoStoreAPI
 }
