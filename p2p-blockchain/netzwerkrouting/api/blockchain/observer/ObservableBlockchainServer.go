2b110276efc921c9a3caef4981cc4ea7f75b950a
Bennet <bennetkrzenzck@pm.me>
2026-01-18 23:14:20 +0100

    Remove unused NotifyMerkleBlock and NotifySetFilterRequest methods from ObservableBlockchainServer

--- p2p-blockchain/netzwerkrouting/api/blockchain/observer/ObservableBlockchainServer.go
+++ p2p-blockchain/netzwerkrouting/api/blockchain/observer/ObservableBlockchainServer.go
@@ -19,10 +19,8 @@ type ObservableBlockchainServerAPI interface {
 	NotifyInv(inventory []*inv.InvVector, peerID common.PeerId)
 	NotifyGetData(inventory []*inv.InvVector, peerID common.PeerId)
 	NotifyBlock(block block.Block, peerID common.PeerId)
-	NotifyMerkleBlock(merkleBlock block.MerkleBlock, peerID common.PeerId)
 	NotifyTx(tx transaction.Transaction, peerID common.PeerId)
 	NotifyGetHeaders(locator block.BlockLocator, peerID common.PeerId)
 	NotifyHeaders(headers []*block.BlockHeader, peerID common.PeerId)
-	NotifySetFilterRequest(setFilterRequest block.SetFilterRequest, peerID common.PeerId)
 	NotifyMempool(peerID common.PeerId)
 }
